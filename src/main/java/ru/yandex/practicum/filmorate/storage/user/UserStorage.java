a096e813f312dcacd4b121856c806f5d6c91c1fd
Макс <maksim5494@yandex.ru>
2026-03-30 22:55:34 +0500

    13

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -6,9 +6,13 @@ import java.util.Collection;
 public interface UserStorage {
 
     User addUser(User user);
+
     void removeUser(Long id);
+
     User modifyUser(User user);
+
     User findUserById(long id);
+
     Collection<User> getAllUsers();
 
 }
