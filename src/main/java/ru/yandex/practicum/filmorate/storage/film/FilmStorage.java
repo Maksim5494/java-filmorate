595448f3a817071066c133bd8c28b597527acb74
Макс <maksim5494@yandex.ru>
2026-03-30 22:54:43 +0500

    12

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -4,9 +4,15 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.util.Collection;
 
 public interface FilmStorage {
+
     Film addFilm(Film film);
+
     void removeFilm(Long id);
+
     Film modifyFilm(Film film);
+
     Collection<Film> getAllFilms();
+
     Film findFilmById(long id);
+
 }
