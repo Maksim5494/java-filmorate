6f6c71d9a21fa69a4e6e0214638a00bfb16da91a
Макс <maksim5494@yandex.ru>
2026-04-03 19:49:41 +0500

    20

--- src/test/java/ru/yandex/practicum/filmorate/UserValidationTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserValidationTest.java
@@ -1,17 +1,18 @@
 package ru.yandex.practicum.filmorate;
 
 import jakarta.validation.ConstraintViolation;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 import ru.yandex.practicum.filmorate.model.User;
 import jakarta.validation.Validator;
 import java.util.Set;
-
-import static org.assertj.core.api.Fail.fail;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
@@ -23,6 +24,16 @@ class UserValidationTest {
     @Autowired
     private Validator validator;
 
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
     @Test
     void testUserValidation() {
         User user = new User();
@@ -33,25 +44,15 @@ class UserValidationTest {
         assertThat(violations).isNotEmpty();
     }
 
-    @Autowired
-    private MockMvc mockMvc;
-
     @Test
     void testAddFriendWithUnknownUser() {
         long existingUserId = 1L;
         long unknownFriendId = 999L;
 
-        try {
-            ResultActions result = mockMvc.perform(
-                    put("/users/" + existingUserId + "/friends/" + unknownFriendId)
-            );
-            result.andDo(MockMvcResultHandlers.print())
-                    .andExpect(status().isNotFound());
-        } catch (Exception e) {
-            e.printStackTrace(); // выведет стектрейс ошибки
-            fail("Test failed: " + e.getMessage());
-        }
+        ResultActions result = mockMvc.perform(
+                put("/users/" + existingUserId + "/friends/" + unknownFriendId)
+        );
+        result.andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isNotFound());
     }
-
 }
-
