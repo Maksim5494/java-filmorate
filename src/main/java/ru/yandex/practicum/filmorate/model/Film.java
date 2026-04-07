a52cc35576a7eb877cc53a3291375b47dd7bd643
Макс <maksim5494@yandex.ru>
2026-04-07 21:54:56 +0500

    12

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,10 +1,8 @@
 package ru.yandex.practicum.filmorate.model;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+
 import java.time.LocalDate;
 
 @Data
@@ -17,7 +15,7 @@ public class Film {
     @Size(max = 200, message = "Описание не может превышать 200 символов")
     private String description;
 
-    @NotNull(message = "Дата релиза должна быть указана")
+    @FutureOrPresent(message = "Дата релиза не может быть в прошлом")
     private LocalDate releaseDate;
 
     @Positive(message = "Продолжительность фильма должна быть положительной")
